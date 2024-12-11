b5255ac4c220249c9c8e75bbc4cc30a456c0df68
EliaGiolli <146369060+EliaGiolli@users.noreply.github.com>
2024-12-11 20:58:26 +0100

    Add files via upload

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,4 +1,21 @@
 function toggleMenu() {
     const mobileMenu = document.getElementById('mobile-menu');
     mobileMenu.classList.toggle('hidden');
-  }
+}
+
+const cardContainer = document.querySelector('.card-container');
+cardContainer.addEventListener('click', function(e) {
+    if (e.target.tagName === 'BUTTON') {
+        const hiddenContent = this.querySelector('.hidden');
+        
+        if (hiddenContent) {
+            hiddenContent.classList.toggle('hidden');
+            hiddenContent.classList.toggle('opacity-0');  
+            hiddenContent.classList.toggle('max-h-0');   
+            
+            e.target.textContent = hiddenContent.classList.contains('hidden') ? 
+                'Scopri di più' : 'Mostra meno';
+        }
+    }
+});
+
